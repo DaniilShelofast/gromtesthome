c7856902263fa94604f16533c19dd8c4bcf064ab
DaniilShelofast <shelofast.daniil@gmail.com>
2022-12-13 23:55:10 +0200

    new date

--- src/lesson20/task2/Demo.java
+++ src/lesson20/task2/Demo.java
@@ -1,22 +1,16 @@
 package lesson20.task2;
 
-import lesson20.task2.exception.BadRequestException;
-
 import java.util.Arrays;
 import java.util.Date;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
-        Utils utils = new Utils();
-        Controller controller = new Controller();
+
+
         Transaction transaction = new Transaction(12, "Odessa", 40, "money", TransactionType.INCOME, new Date());
-        controller.save(transaction);
-        System.out.println(controller.save(transaction));
-        //System.out.println(Arrays.toString(controller.transactionList()));
-
-
-
-
+        System.out.println(Controller.save(transaction));
+        //System.out.println(Arrays.toString(Controller.transactionList()));
+        
 
     }
 }

35d69befd2c2262d263eb014c035dfce85095d9b
DaniilShelofast <shelofast.daniil@gmail.com>
2022-12-23 10:36:50 +0200

    new date

--- src/lesson26/task/ArrayListTest.java
+++ src/lesson26/task/ArrayListTest.java
@@ -2,30 +2,50 @@ package lesson26.task;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListTest {
     public static void main(String[] args) {
-        Order order = new Order(1, 100, "USA", "microcircuit", "ITM");
         ArrayListTest arrayListTest = new ArrayListTest();
-        System.out.println(arrayListTest.useList(order));
+
+        System.out.println(arrayListTest.useList());
     }
 
-    public Order useList(Order order) {
+    public ArrayList<Order> useList() {
+        Order order = new Order(1, 100, "USA", "microcircuit", "ITM");
         ArrayList<Order> list = new ArrayList<>();
+
         list.add(order);
+        System.out.println(list);
+
         list.add(0, order);
+        System.out.println(list);
+
         list.set(1, order);
+        System.out.println(list);
+
         list.remove(0);
+        System.out.println(list);
+
         list.subList(0, 1);
+        System.out.println(list);
+
         list.remove(order);
         System.out.println(list.contains(order));
+
         Order[] orders = list.toArray(new Order[0]);
+        System.out.println(Arrays.deepToString(orders));
+
         list.clear();
+        System.out.println(list);
 
+        Order secondOrder = new Order(12, 200, "USA", "microcircuit", "BMW");
         ArrayList<Order> secondList = new ArrayList<>();
-        secondList.addAll(0,list);
+        secondList.add(secondOrder);
 
-        return order;
+        list.addAll(secondList);
+
+        return list;
     }
 
 }

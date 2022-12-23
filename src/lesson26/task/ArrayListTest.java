68ecc49b8deb7897e3fff7c956fcf02296d0f7c1
DaniilShelofast <shelofast.daniil@gmail.com>
2022-12-23 23:09:42 +0200

    new date

--- src/lesson26/task/ArrayListTest.java
+++ src/lesson26/task/ArrayListTest.java
@@ -12,53 +12,40 @@ public class ArrayListTest {
 
     public ArrayList<Order> useList() {
         Order order = new Order(1, 100, "USA", "microcircuit", "GGG");
-        Order eighthOrder = new Order(8, 150, "USA", "microcircuit", "AudiQ8");
-        Order ninthOrder = new Order(9, 300, "USA", "microcircuit", "Reno");
-        Order seventhOrder = new Order(7, 170, "USA", "microcircuit", "MercedesGlM");
-        ArrayList<Order> list = new ArrayList<>();
-
-        list.add(order);
-        list.add(eighthOrder);
-        list.add(seventhOrder);
-        list.add(ninthOrder);
-
-
-        list.add(1, seventhOrder);
-        System.out.println(list);
-
-        list.set(1, order);
-        System.out.println(list);
-
-        list.remove(2);
-        System.out.println(list);
-
-
-        list.subList(0, 1);
-        System.out.println(list);
-
-        list.remove(order);
-        System.out.println(list.contains(ninthOrder));
-
-        Order[] orders = list.toArray(new Order[4]);
-        System.out.println(Arrays.deepToString(orders));
-
-        list.clear();
-        System.out.println(list);
-
         Order secondOrder = new Order(2, 170, "USA", "microcircuit", "Lada");
         Order thirdOrder = new Order(3, 150, "USA", "microcircuit", "Audi");
         Order fourthOrder = new Order(4, 300, "USA", "microcircuit", "Volvo");
+        ArrayList<Order> list = new ArrayList<>();
+
+        list.add(order);
+        list.add(1, secondOrder);
+        list.add(thirdOrder);
+        list.add(fourthOrder);
+
+        list.set(2, order);
+        list.remove(1);
+        list.subList(0, 2).clear();
+        list.remove(order);
+        System.out.println(list.contains(fourthOrder));
+        Order[] orders = list.toArray(new Order[5]);
+        System.out.println(Arrays.deepToString(orders));
+        list.clear();
+
+        ArrayList<Order> secondList = new ArrayList<>();
         Order fifthOrder = new Order(5, 170, "USA", "microcircuit", "Mercedes");
         Order sixthOrder = new Order(6, 200, "USA", "microcircuit", "BMW");
-        ArrayList<Order> secondList = new ArrayList<>();
-        secondList.add(sixthOrder);
-        secondList.add(secondOrder);
-        secondList.add(thirdOrder);
-        secondList.add(fourthOrder);
+        Order seventhOrder = new Order(7, 170, "USA", "microcircuit", "Volvo");
+        Order eighthOrder = new Order(8, 70, "USA", "microcircuit", "MercedesGlM");
+        Order ninthOrder = new Order(9, 17, "USA", "microcircuit", "Reno");
+
         secondList.add(fifthOrder);
+        secondList.add(sixthOrder);
+        secondList.add(seventhOrder);
 
         list.addAll(secondList);
 
+        list.add(eighthOrder);
+        list.add(0,ninthOrder);
 
         return list;
     }

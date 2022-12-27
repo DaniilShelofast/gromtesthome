d3187f3ee5959fc76aa6e4762c2f12b262b7d147
DaniilShelofast <shelofast.daniil@gmail.com>
2022-12-27 19:31:12 +0200

    new date

--- src/lesson27/task2/UserDAO.java
+++ src/lesson27/task2/UserDAO.java
@@ -1,93 +1,136 @@
 package lesson27.task2;
 
+import lesson22.repository.User;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 //
 public class UserDAO {
 
-    public static ArrayList<User> findById(long id) throws Exception {
-        User user1 = new User(1, "User1", "one");
-        User user2 = new User(2, "User2", "one");
-        User user3 = new User(3, "User3", "one");
+    private final static User[] users = new User[10];
 
-        ArrayList<User> list = new ArrayList<>();
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
+    public static User[] getUsers() {
 
-        for (User user : list) {
-            if (user != null && user.getId() == id) {
-
-                return list;
-            }
-        }
-        throw new Exception("User with id: " + id + " not found");
+        return users;
     }
 
 
-    public static void delete(long id) {
-        LinkedList<User> list = new LinkedList<>();
-        User user1 = new User(1, "User1", "one");
-        User user2 = new User(2, "User2", "one");
-        User user3 = new User(3, "User3", "one");
-
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) != null && id == list.get(i).getId()) {
-                list.remove(i);
+    public static LinkedList<String> getUserNames() {
+        LinkedList<String> linkedList = new LinkedList<>();
+        String[] userNames = new String[users.length];
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null) {
+                userNames[i] = users[i].getName();
             }
         }
+        return linkedList;
     }
 
-    public static ArrayList<User> upDate(User user) throws Exception {
-
-        User user1 = new User(1, "User1", "one");
-        User user2 = new User(2, "User2", "one");
-        User user3 = new User(3, "User3", "one");
-
-        ArrayList<User> list = new ArrayList<>();
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) != null && user.getId() == list.get(i).getId()) {
-                list.add(user);
-
-                return list;
+    public static LinkedList<Long> getUserIds() {
+        LinkedList<Long> linkedList = new LinkedList<>();
+        long[] it = new long[users.length];
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null) {
+                it[i] = users[i].getId();
             }
         }
-        throw new Exception("Unexpected error");
+        return linkedList;
     }
 
-    public static LinkedList<User> saveUser(User user) throws Exception {
-        LinkedList<User> list = new LinkedList<>();
-        User user1 = new User(1, "User1", "one");
-        User user2 = new User(2, "User2", "one");
-        User user3 = new User(3, "User3", "one");
+    public static ArrayList<String> getUserNameById(long id) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (User it : users) {
+            if (id == it.getId()) {
 
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
+                return arrayList;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<User> getUserByName(String name) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (User it : users) {
+            if (name.equals(it.getName())) {
+
+                return arrayList;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<User> getUserById(long id) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (User it : users) {
+            if (id == it.getId()) {
+
+                return arrayList;
+            }
+        }
+        return null;
+    }
+
+    public static ArrayList<User> getUserBySessionId(String sessionId) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (User it : users) {
+            if (sessionId.equals(it.getSessionId())) {
+                return arrayList;
+            }
+        }
+        return null;
+    }
+
+
+    public static ArrayList<User> findById(long id) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (User it : users) {
+            if (it != null && id == it.getId()) {
+                return arrayList;
+            }
+        }
+        return null;
+    }
+
+
+    public static LinkedList<User> save(User user) {
+        LinkedList<User> linkedList = new LinkedList<>();
 
         ArrayList<User> findById = findById(user.getId());
         if (findById != null) {
-            throw new Exception("Can`t save null user");
+            return null;
         } else {
-
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) == null) {
-                    list.add(user);
-                    return list;
+            for (int i = 0; i < users.length; i++) {
+                if (users[i] == null) {
+                    users[i] = user;
+                    return linkedList;
                 }
             }
-            throw new Exception("Not space to save user with id: " + user.getId());
+            return null;
         }
 
+    }
+
+
+    public static ArrayList<User> upDate(User user) {
+        ArrayList<User> arrayList = new ArrayList<>();
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null && user.getId() == users[i].getId()) {
+                users[i] = user;
+                return arrayList;
+            }
+
+        }
+        return null;
+    }
+
+    public static void delete(long id) {
+
+
+        for (int i = 0; i < users.length; i++) {
+            if (users[i] != null && id == users[i].getId()) {
+                users[i] = null;
+            }
+        }
     }
 }

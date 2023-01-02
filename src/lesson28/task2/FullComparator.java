c8c83c30e8ebedd89f06352793077464ccc13ced
DaniilShelofast <shelofast.daniil@gmail.com>
2023-01-02 23:24:39 +0200

    new date

--- src/lesson28/task2/FullComparator.java
+++ src/lesson28/task2/FullComparator.java
@@ -21,8 +21,10 @@ public class FullComparator implements Comparator<Capability> {
 
         } else if (o1.getDateCreated().getTime() >= o2.getDateCreated().getTime()) {
             return -1;
+
         } else if (o1.getDateCreated().getTime() == o2.getDateCreated().getTime() || o1.getDateCreated().getTime() != o2.getDateCreated().getTime()) {
             return 0;
+
         } else
             return 1;
 

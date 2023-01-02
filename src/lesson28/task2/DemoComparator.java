7f2911ece1137c3fe4216a34a307206682fd9256
DaniilShelofast <shelofast.daniil@gmail.com>
2023-01-02 23:52:04 +0200

    new date

--- src/lesson28/task2/DemoComparator.java
+++ src/lesson28/task2/DemoComparator.java
@@ -8,16 +8,13 @@ import java.util.Date;
 
 public class DemoComparator {
     public static void main(String[] args) {
-        Capability capability1 = new Capability(1001, "a", "l", true, new Date());
-        Capability capability2 = new Capability(1002, "a", "l", true, new Date(109, 7, 4, 11, 15, 0));
-        Capability capability3 = new Capability(1003, "a", "l", true, new Date(107, 5, 4, 12, 15, 0));
-        Capability capability4 = new Capability(1004, "a", "l", true, new Date());
         ArrayList<Capability> capabilities = new ArrayList<>();
 
-        capabilities.add(capability1);
-        capabilities.add(capability2);
-        capabilities.add(capability3);
-        capabilities.add(capability4);
+        capabilities.add(new Capability(1001, "a", "l", true, new Date()));
+        capabilities.add(new Capability(1002, "a", "l", true, new Date(109, 7, 4, 11, 15, 0)));
+        capabilities.add(new Capability(1003, "a", "l", true, new Date(107, 5, 4, 12, 15, 0)));
+        capabilities.add(new Capability(1004, "a", "l", true, new Date()));
+
         System.out.println(capabilities);
         System.out.println("--");
         capabilities.sort(new FullComparator());

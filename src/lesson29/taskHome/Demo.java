2b54b48426eccecd7f391567c86cbcba63dcf83b
DaniilShelofast <shelofast.daniil@gmail.com>
2023-01-04 15:11:05 +0200

    new date

--- src/lesson29/taskHome/Demo.java
+++ src/lesson29/taskHome/Demo.java
@@ -3,5 +3,6 @@ package lesson29.taskHome;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(HashSetTest.useHashSet());
+        System.out.println("");
     }
 }

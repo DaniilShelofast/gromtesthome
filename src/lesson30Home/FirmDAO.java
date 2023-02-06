38fda6132eafef37819962438c41eb78e5e71d04
DaniilShelofast <shelofast.daniil@gmail.com>
2023-02-07 00:45:38 +0200

    new date

--- src/lesson30Home/FirmDAO.java
+++ src/lesson30Home/FirmDAO.java
@@ -1,25 +1,13 @@
 package lesson30Home;
-
-
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 public class FirmDAO {
-
-    private static final Set<Firm> firms = new HashSet<>();
-
 
     private FirmDAO() {
 
         Firm firm1 = new Firm(new Date(121, 10, 15));
         Firm firm2 = new Firm(new Date(100, 15, 10));
-        firms.add(firm1);
-        firms.add(firm2);
-    }
 
-    public Set<Firm> getFirms() {
-        return firms;
     }
 
 }

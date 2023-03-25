c82a9bb524ba6a5a044f3c38f32e9d66760f6579
DaniilShelofast <shelofast.daniil@gmail.com>
2023-03-25 22:30:12 +0200

    UPD Done task home.

--- src/lesson31/Solution.java
+++ src/lesson31/Solution.java
@@ -7,12 +7,11 @@ public class Solution {
 
     public static Map<Character, Integer> countSymbols(String text) {
         Map<Character, Integer> map = new HashMap<>();
-
-        int count = 0;
         for (char symbol : text.toCharArray()) {
             if (!map.containsKey(symbol)) {
                 map.put(symbol, 1);
             } else {
+                int count = 0;
                 count = map.get(symbol);
                 map.put(symbol, count + 1);
             }
@@ -22,12 +21,11 @@ public class Solution {
 
     public static Map<String, Integer> words(String text) {
         Map<String, Integer> map = new HashMap<>();
-        int count = 0;
-
         for (String string : text.split(" ")) {
             if (!map.containsKey(string)) {
                 map.put(string, 1);
             } else {
+                int count = 0;
                 count = map.get(string);
                 map.put(string, count + 1);
             }
